3b2861701787f512e8dc8b9da847c24c0e4631ca
M.Windeker <yizzr81@gmail.com>
2024-03-02 15:15:20 +0500

    add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,89 @@
 /* eslint-env node */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    '@nuxt/eslint-config',
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  root: true,
-  rules: {
-    'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2],
-    'key-spacing': ['error', {
-      beforeColon: false,
-      afterColon: true,
-    }],
-    'keyword-spacing': ['error', {
-      before: true,
-      after: true,
-    }],
-    quotes: ['error', 'single'],
-    'object-curly-spacing': ['error', 'always'],
-    semi: ['error', 'never'],
-    'space-before-blocks': ['error', 'always'],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'never',
-    }],
-    'vue/no-v-html': 'off',
-    'vue/script-indent': ['error', 2, {
-      baseIndent: 1,
-      switchCase: 1,
-    }],
-    'no-irregular-whitespace': 'off',
-  },
-  overrides: [{
-    files: ['*.vue'],
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-recommended',
+        '@nuxt/eslint-config',
+        'plugin:prettier/recommended',
+        'prettier',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    root: true,
     rules: {
-      indent: 'off',
+        'comma-dangle': ['error', 'always-multiline'],
+        indent: ['error', 4],
+        'key-spacing': [
+            'error',
+            {
+                beforeColon: false,
+                afterColon: true,
+            },
+        ],
+        'keyword-spacing': [
+            'error',
+            {
+                before: true,
+                after: true,
+            },
+        ],
+        quotes: ['error', 'single'],
+        'object-curly-spacing': ['error', 'always'],
+        semi: ['error', 'never'],
+        'space-before-blocks': ['error', 'always'],
+        'vue/html-closing-bracket-newline': [
+            'error',
+            {
+                singleline: 'never',
+                multiline: 'never',
+            },
+        ],
+        'vue/html-indent': 'off',
+        'vue/no-v-html': 'off',
+        'vue/script-indent': [
+            'error',
+            4,
+            {
+                baseIndent: 0,
+                switchCase: 1,
+            },
+        ],
+        'no-irregular-whitespace': 'off',
+        'vue/attributes-order': [
+            'error',
+            {
+                order: [
+                    'DEFINITION',
+                    'LIST_RENDERING',
+                    'CONDITIONALS',
+                    'RENDER_MODIFIERS',
+                    'GLOBAL',
+                    ['UNIQUE', 'SLOT'],
+                    'TWO_WAY_BINDING',
+                    'OTHER_DIRECTIVES',
+                    'OTHER_ATTR',
+                    'EVENTS',
+                    'CONTENT',
+                ],
+                alphabetical: true,
+            },
+        ],
     },
-  }],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts'],
-      },
+    overrides: [
+        {
+            files: ['*.vue'],
+            rules: {
+                indent: 'off',
+            },
+        },
+    ],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.ts'],
+            },
+        },
     },
-  },
 }
--- components/main/ProjectCard.vue
+++ components/main/ProjectCard.vue
@@ -1,39 +1,46 @@
 <script setup>
-  defineProps({
+defineProps({
     project: {
-      type: Object,
-      required: true,
+        type: Object,
+        required: true,
     },
-  })
+})
 </script>
 
 <template>
-  <div class="card">
-    <div class="card-image">
-      <img
-        :src="project.image"
-        alt="Placeholder image">
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-content">
-          <p class="title is-4">
-            {{ project.title }}
-          </p>
-          <p class="subtitle is-6">
-            {{ project.subtitle }}
-          </p>
+    <div class="card">
+        <div class="card-image">
+            <img
+                :src="project.image"
+                alt="Placeholder image" />
         </div>
-      </div>
-      <div class="content">
-        {{ project.description }}
-        <br>
-        <a
-          :href="project.link"
-          target="_blank">Learn more</a>
-      </div>
+        <div class="card-content">
+            <div class="media">
+                <div class="media-content">
+                    <p class="title is-4">
+                        {{ project.title }}
+                    </p>
+                    <p class="subtitle is-6">
+                        {{ project.subtitle }}
+                    </p>
+                </div>
+            </div>
+            <div class="content">
+                {{ project.description }}
+                <br />
+                <a
+                    id="ten1"
+                    class="flex">
+                    learn more
+                </a>
+                <a
+                    :href="project.link"
+                    target="_blank"
+                    >Learn more</a
+                >
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style></style>
