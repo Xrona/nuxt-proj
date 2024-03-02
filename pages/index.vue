e260dc83d5ae039c1c9d530f06e6e30163e3a197
M.Windeker <yizzr81@gmail.com>
2024-03-02 13:17:25 +0500

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+const project = ref({
+  title: 'My project',
+  subtitle: 'Subtitle',
+  description: 'Description',
+  image: 'https://images.unsplash.com/photo-1708649290066-5f617003b93f?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  link: 'https://example.com',
+});
+</script>
+
+<template>
+  <div>
+    <h1>Hello world index page</h1>
+    <MainProjectCard project />
+  </div>
+</template>
+
+<style ></style>
