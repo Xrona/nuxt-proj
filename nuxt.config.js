ab873468f71e9f8cedf821bcd5a9adcc0307013b
Nikita Alexov <polarbeararmor@gmail.com>
2024-03-02 14:34:29 +0400

    chore: add tailwindcss, update project structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 // eslint-disable-next-line
 export default defineNuxtConfig({
+  srcDir: './src',
   devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/main.css'],
 })
